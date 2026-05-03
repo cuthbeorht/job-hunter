63849b80a7e69a5e04d9785873ee3dd9f12eba41
David Sciacchettano <david.sciacchettano@gmail.com>
2026-05-03 16:29:59 -0400

    Fix more imports

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,9 +1,10 @@
 import { configDefaults, defineConfig } from "vitest/config";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     test: {
         exclude: [...configDefaults.exclude, "dist", "node_modules"],
     },
-    plugins: [tsConfigPaths()],
+    resolve: {
+        tsconfigPaths: true,
+    }
 });
