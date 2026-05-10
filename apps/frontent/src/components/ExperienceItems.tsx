ee2bf1eeaecb94c96e55eac7c88e8567d9488ffc
David Sciacchettano <david.sciacchettano@gmail.com>
2026-05-10 19:33:41 -0400

    WIP

--- apps/frontent/src/components/ExperienceItems.tsx
+++ apps/frontent/src/components/ExperienceItems.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export default function ExperienceItems() {
     
+    const [newExperienceItem, setNewExperienceItem] = useState("");
+    const [hint, setHint] = useState("");
+
+    function handleExperienceChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setNewExperienceItem(event.target.value);
+    }
+
+    function handleSubmit() {
+        // Here you would typically send the new experience item to your backend API
+        console.log("New Experience Item:", newExperienceItem);
+        if (newExperienceItem.trim() === "") {
+            setHint("Experience item cannot be empty.");
+            return;
+        }
+        setNewExperienceItem(""); // Clear the input after submission
+    }
     
   
     return (
-        <div className="card">
-        <h2>Experience Items</h2>
-        <p>Here you can manage your experience items.</p>
+        <div className="">
+            <h2>Experience Items</h2>
+            <p>Here you can manage your experience items.</p>
+            <label htmlFor="experience">Experience:</label>
+            <input type="text" placeholder = {hint} id="experience" name="experience" value={newExperienceItem} onChange={handleExperienceChange} />
+            <button className="" onClick={handleSubmit}>Add Experience</button>
         </div>
     );
 }
---
+++ apps/frontent/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <nav className="">
+      <div className="">
+        <div className="">Job Hunter</div>
+        <div>
+          <a href="/" className="">Home</a>
+          <a href="/work-experience" className="">Work Experience</a>
+          <a href="/about" className="">About</a>
+          <a href="/contact" className="">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
