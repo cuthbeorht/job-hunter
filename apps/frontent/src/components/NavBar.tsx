3f1f7f61c3f009123277d348707dff8b2a56dec0
David Sciacchettano <david.sciacchettano@gmail.com>
2026-05-10 20:13:35 -0400

    Get simple meny up and running

--- apps/frontent/src/components/NavBar.tsx
+++ apps/frontent/src/components/NavBar.tsx
@@ -1,15 +1,17 @@
+import './NavBar.css'
+
 export default function NavBar() {
   return (
-    <nav className="">
-      <div className="">
-        <div className="">Job Hunter</div>
-        <div>
-          <a href="/" className="">Home</a>
-          <a href="/work-experience" className="">Work Experience</a>
-          <a href="/about" className="">About</a>
-          <a href="/contact" className="">Contact</a>
-        </div>
-      </div>
+    <nav className="navbar-wrapper">
+    
+        <div className="navbar-title">Job Hunter</div>
+        <ul className="navbar-list">
+          <li><a href="/" className="navbar-link">Home</a></li>
+          <li><a href="/work-experience" className="navbar-link">Work Experience</a></li>
+          <li><a href="/about" className="navbar-link">About</a></li>
+          <li><a href="/contact" className="navbar-link">Contact</a></li>
+        </ul>
+      
     </nav>
   );
 }
